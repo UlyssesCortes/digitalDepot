e418c37c66d03a8f1e40b700124b9231d4e071e4
root <root@DESKTOP-30JE0LL>
2023-05-01 10:32:50 -0500

    products table initiated

---
+++ server/models/index.js
@@ -0,0 +1,8 @@
+module.exports = {
+    // User: require("./user"),
+    // OrderItems: require("./order-items"),
+    Product: require("./product"),
+    // Order: require("./order"),
+    // Review: require("./review"),
+};
+
