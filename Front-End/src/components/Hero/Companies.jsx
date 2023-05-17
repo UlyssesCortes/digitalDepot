84b3c2e4d5fcd9880da9858de9e51bea75fdc682
root <root@DESKTOP-30JE0LL>
2023-05-16 21:43:12 -0500

    companies logos added still need to be correctly styled

---
+++ Front-End/src/components/Hero/Companies.jsx
@@ -0,0 +1,12 @@
+
+export default function Companies() {
+    return (
+        <section className="conpaniesContainer">
+            <div className='huobi'></div>
+            <div className='fastmpany'></div>
+            <div className='duofast'></div>
+            <div className='hornet'></div>
+            <div className='chase'></div>
+        </section>
+    )
+}
--- Front-End/src/components/Hero/Hero.jsx
+++ Front-End/src/components/Hero/Hero.jsx
@@ -1,11 +1,13 @@
 import '../../css/hero.css'
 
 import Banner from "./Banner"
+import Companies from './Companies'
 
 export default function Hero() {
     return (
         <>
             <Banner />
+            <Companies />
         </>
     )
 }
